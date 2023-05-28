4e5d699f51061e2cf1577ac52bb3ea7afd12670f
freddymani22 <freddymani.fm@gmail.com>
2023-05-29 00:15:42 +0530

    allowed hosts set to all.

--- django_chat/settings.py
+++ django_chat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY =  os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG'))=='1'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -73,6 +73,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'django_chat.wsgi.application'
 ASGI_APPLICATION = 'django_chat.asgi.application'

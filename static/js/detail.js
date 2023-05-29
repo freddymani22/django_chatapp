40334d74f33a2d7191895d82887e8c4b57822f62
freddymani22 <freddymani.fm@gmail.com>
2023-05-29 14:10:49 +0530

    updated websocket.

--- static/js/detail.js
+++ static/js/detail.js
@@ -1,7 +1,7 @@
 const chatRoomName = JSON.parse(document.querySelector('#json-chatroomname').textContent)
 const username = JSON.parse(document.querySelector('#json-username').textContent)
 const chatSocket = new WebSocket(
-    'ws://'
+    'wss://'
     + window.location.host
     + '/ws/'
     + chatRoomName
